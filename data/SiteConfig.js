a83b9eb6eb340224840f8a61056466b75c694575
Anai Araya <anai.yusary@gmail.com>
2018-03-20 09:52:17 +1100

    fix path prefix

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -4,7 +4,7 @@ module.exports = {
   siteTitleAlt: "Soggy Ink", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
   siteUrl: "https://soggy-ink.surge.sh", // Domain of your website without pathPrefix.
-  pathPrefix: "", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
+  pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
   fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
   siteDescription:
     "Articles on Javascript, React, Node.js, CSS Grid, the walls I beat my head against and the struggle on this beautiful journey towards mastering my craft.", // Website description used for RSS feeds/meta description tag.
